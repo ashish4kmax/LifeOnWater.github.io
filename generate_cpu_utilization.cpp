0e41c2bfb10f0916137ec75748cf5f848e2a90e0
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-11-27 16:09:11 +0530

    Update generate_cpu_utilization.cpp

--- generate_cpu_utilization.cpp
+++ generate_cpu_utilization.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <sys/sysinfo.h>
+#include <chrono>
+#include <thread>
 
 int main() {
-  // Get the current CPU utilization
-  struct sysinfo si;
-  sysinfo(&si);
+    const int loopIterations = 100000000;
 
-  // Calculate CPU utilization percentage
-  double cpu_usage = si.cpu_total - si.cpu_idle;
-  double cpu_percent = cpu_usage / si.cpus * 100;
+    while (true) {
+        // Simulate CPU work
+        for (int i = 0; i < loopIterations; ++i) {
+            // Do some computation
+        }
 
-  // Convert CPU percentage to integer
-  int cpu_utilization = (int)cpu_percent;
+        // Sleep for a short duration to avoid 100% CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
-  // Print CPU utilization
-  std::cout << "CPU utilization: " << cpu_utilization << "%" << std::endl;
-
-  return 0;
+    return 0;
 }
